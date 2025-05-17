9f7575ab78b70f3cfabddcf070fa2ce4cf1ba020
mharkservices <157296975+mharkservices@users.noreply.github.com>
2025-05-18 00:22:10 +0530

    Update compliance-calendar.tsx

--- client/src/pages/compliance-calendar.tsx
+++ client/src/pages/compliance-calendar.tsx
@@ -174,8 +174,8 @@ const ComplianceCalendar = () => {
                 <SelectValue placeholder="Select Year" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="2023">2023-2024</SelectItem>
-                <SelectItem value="2024">2024-2025</SelectItem>
+                <SelectItem value="2025">2025-2026</SelectItem>
+                <SelectItem value="2026">2026-2027</SelectItem>
               </SelectContent>
             </Select>
           </div>
